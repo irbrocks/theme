44472b89d1553658e9ed6c1ed7db11cc93a439e3
Angel M de Miguel <angelmm@redborder.net>
2015-06-27 14:02:33 +0200

    Added Gulp to build .css files

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('default', function() {
+  gulp.start('sass:watch');
+});
+
+gulp.task('sass', function () {
+  gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('irb.blocks.css'))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:production', function (){
+  gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('irb.blocks.min.css'))
+    .pipe(gulp.dest('./assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
