6599b9ee5fb7bca2517bc173a4f9e7474818c25a
Angel M de Miguel <angelmm@redborder.net>
2015-07-20 23:53:43 +0200

    Updated UI. Added changes to post list and post content. Created multiple SCSS files

---
+++ assets/js/irb.rocks.js
@@ -0,0 +1,24 @@
+/*
+ * Main file of IRB Rocks!
+ */
+
+$(document).ready(function(){
+
+  // Future
+  /*$('.post-link').click(function(e){
+    var el = $(this);
+    e.preventDefault();
+
+    // Hide the main
+    $('#content').fadeOut(150, function(){
+      // Load async
+      $.ajax({
+        url: el.attr('href')
+      }).done(function(data){
+        window.history.pushState(el.text(), el.text(), el.attr('href'));
+        $('#content').html($(data).find('main#content').html());
+        $('#content').fadeIn(150);
+      });
+    });
+  });*/
+});
